f4690ed74fc9a3fb770adac4eed76471462d3fb5
Wang Cong <ousatoru@hotmail.com>
2019-09-09 17:27:31 +0800

    to exam fr

--- src/api/examAdd.js
+++ src/api/examAdd.js
@@ -2,8 +2,15 @@ import request from '@/utils/request'
 
 export function examAdd(data) {
   return request({
-    url: '/users/create',
+    url: '/examAdd',
     method: 'post',
     data
+  })
+}
+
+export function getSubjects() {
+  return request({
+    url: '/subjects',
+    method: 'get'
   })
 }
--- src/views/form/exam.vue
+++ src/views/form/exam.vue
@@ -5,42 +5,24 @@
         <el-input v-model="form.examName" />
       </el-form-item>
       <el-form-item label="创建人">
-        <el-input v-model="form.createUser" />
+        <el-input v-model="form.createUser" disabled="disabled" />
       </el-form-item>
       <el-form-item label="考试科目">
         <el-select v-model="form.subject" placeholder="请选择考试科目">
+          <el-option v-for="subj in subjects" :key="subj.id" :label="subj.subjName" :value="subj.id" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="考试年级">
+        <el-select v-model="form.grade" placeholder="请选择考试科目">
           <el-option label="Zone one" value="shanghai" />
           <el-option label="Zone two" value="beijing" />
         </el-select>
       </el-form-item>
-      <el-form-item label="Activity time">
-        <el-col :span="11">
-          <el-date-picker v-model="form.date1" type="date" placeholder="Pick a date" style="width: 100%;" />
-        </el-col>
-        <el-col :span="2" class="line">-</el-col>
-        <el-col :span="11">
-          <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
-        </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
-      </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor" />
-          <el-radio label="Venue" />
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
+      <el-form-item label="考试班级">
+        <el-select v-model="form.classes" placeholder="请选择考试科目">
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="onSubmit">创建</el-button>
@@ -52,10 +34,13 @@
 
 <script>
 import { getTokenUser } from '../../utils/auth'
+import { getSubjects } from '../../api/examAdd'
 
 export default {
   data() {
     return {
+      subjects: [],
+      grades: [],
       form: {
         examName: '',
         createUser: getTokenUser(),
@@ -65,7 +50,15 @@ export default {
       }
     }
   },
+  created() {
+    this.getSubject()
+  },
   methods: {
+    getSubject() {
+      getSubjects().then(response => {
+        this.subjects = response.data
+      })
+    },
     onSubmit() {
       this.$message('submit!')
     },
